4d245df6b3413d37d5023f4c1f7e2f03db0b3b51
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-12-14 19:03:57 +0200

    Add project files.

---
+++ computer_architecture_project/HardCodedData.h
@@ -0,0 +1,9 @@
+//This header file holds different macros used in the project.
+
+#ifndef HARD_CODED_DATA_H
+#define HARD_CODED_DATA_H
+
+
+
+
+#endif
---
+++ computer_architecture_project/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+void main() {
+
+
+}
