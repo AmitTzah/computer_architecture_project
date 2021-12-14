fa48510553dce12829776905fb8360830cd0e71d
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-12-14 19:06:36 +0200

    Testing push

--- computer_architecture_project/HardCodedData.h
+++ computer_architecture_project/HardCodedData.h
@@ -1,9 +1,9 @@
-//This header file holds different macros used in the project.
+//This header file holds different macros used in the project, to avoid "magic constants".
 
 #ifndef HARD_CODED_DATA_H
 #define HARD_CODED_DATA_H
 
-
+#define NUMBER_OF_CORES 4
 
 
 #endif
